633688e5308cb168a51f33638da32714b07edc73
Stephane Mensah <ghanafreeware@gmail.com>
2019-03-05 15:50:14 +0000

    added github fork me banner.

---
+++ src/components/ForkMe.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+function ForkMe() {
+    let GithubRepoLink = "https://github.com/waptik/google-keep-reactjs-firebase";
+
+    return (
+                <a href={ GithubRepoLink } target="_blank" rel="noopener noreferrer">
+                    <img alt="Fork me on GitHub" 
+                        data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" 
+                        src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67"
+                        style={{
+                            "position": "absolute",
+                            "right":0,
+                            "border": 0,
+                            "width": "150px"
+                        }}
+                    />
+                </a>
+    );
+}
+
+export default ForkMe;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import logo from './Logo';
 import Title from './Title';
+import ForkMe from './ForkMe';
 
 import '../assets/styles/App.css';
 
 function Header(props) {
     return (
         <div>
-            <Title pageTitle={props.title} />
-            <h1>Google Keep Clone by waptik</h1>
 
-            
-            
-            <div className="logos">
-                <span className="logo keep">
-                    { logo("k") }
-                </span>
+            <header>
+                <ForkMe />
+                <Title pageTitle={props.title} />
+                <h1>Google Keep Clone by waptik</h1>
+                
+                <div className="logos">
+                    <span className="logo keep">
+                        { logo("k") }
+                    </span>
 
-                <span className="plus"> + </span>
+                    <span className="plus"> + </span>
 
-                <span className="logo react">
-                    { logo("r") }
-                </span>
+                    <span className="logo react">
+                        { logo("r") }
+                    </span>
 
-                <span className="plus"> + </span>
+                    <span className="plus"> + </span>
 
-                <span className="logo firebase">
-                    { logo("f") }
-                </span>
-            </div>
+                    <span className="logo firebase">
+                        { logo("f") }
+                    </span>
+                </div>
+            </header>
         </div>
     );
 }
