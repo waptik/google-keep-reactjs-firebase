22d8cc42ceb43f0e309e0f7fe98dcdd4b2750f38
Stephane Mensah <ghanafreeware@gmail.com>
2019-03-05 17:44:48 +0000

    fix environment keys issue in .env and moved it to root folder

--- src/utils/firebase/config.js
+++ src/utils/firebase/config.js
@@ -7,18 +7,18 @@ if (!firebase.apps.length) {
   // Initialize Firebase
   //const settings = {timestampsInSnapshots: true};
   const config = {
-    apiKey: `{process.env.FIREBASE_API_KEY}`,
-    authDomain: `{process.env.FIREBASE_AUTH_DOMAIN}`,
-    databaseURL: `{process.env.FIREBASE_DATABASE_URL}`,
-    projectId: `{process.env.FIREBASE_PROJECT_ID}`,
-    storageBucket: `{process.env.FIREBASE_STORAGE_BUCKET}`,
-    messagingSenderId: `{process.env.FIREBASE_MESSAGING_SENDER_ID}`,
+    apiKey: `{process.env.REACT_APP_FIREBASE_API_KEY}`,
+    authDomain: `{process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+    databaseURL: `{process.env.REACT_APP_FIREBASE_DATABASE_URL}`,
+    projectId: `{process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+    storageBucket: `{process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+    messagingSenderId: `{process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
   };
 
   firebase.initializeApp(config);
   //firebase.firestore().settings(settings);
 
-  console.log(process.env.FIREBASE_DATABASE_URL)
+  //console.log(process.env) // view environment keys in console log
 }
 
 const db = firebase.database();
